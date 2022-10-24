ec171ac59dbedd0764d674d6ce2a1a029e627561
Lynchk07 <lynch.kristinj@gmail.com>
2022-10-23 23:12:01 -0700

    routes need edited based on insomnia testing

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,6 +8,12 @@ router.get('/', (req, res) => {
   Category.findAll({include:[Product]})
   .then(categoryDataBase => res.json(categoryDataBase)).catch(err => res.json(err))
   // be sure to include its associated Products
+
+  // try {
+  //   //     const userData = await User.update(req.body, {
+  //   //       where: {
+  //   //         id: req.params.id,
+  //   //       },
 });
 
 router.get('/:id', (req, res) => {
@@ -47,3 +53,53 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+// const router = require('express').Router();
+// const User = require('../../models/User');
+
+// // GET a user
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const userData = await User.findByPk(req.params.id);
+//     if (!userData) {
+//       res.status(404).json({ message: 'No user with this id!' });
+//       return;
+//     }
+//     res.status(200).json(userData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// // UPDATE a user
+// router.put('/:id', async (req, res) => {
+//   
+//     });
+//     if (!userData[0]) {
+//       res.status(404).json({ message: 'No user with this id!' });
+//       return;
+//     }
+//     res.status(200).json(userData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// // DELETE a user
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const userData = await User.destroy({
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     if (!userData) {
+//       res.status(404).json({ message: 'No user with this id!' });
+//       return;
+//     }
+//     res.status(200).json(userData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// module.exports = router;
